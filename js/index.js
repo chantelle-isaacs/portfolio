e9e6a6e738c8472f4377dcd9236dadfe1cea5c82
Chantelle Isaacs <chantelle616@gmail.com>
2020-05-30 12:28:55 -0700

    adjust circular text by creating separate divs for circles to live in, adjust fonts to Roboto Mono and Montserrat, renamed color variables and reassigned values in CSS

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 //makes text curve
 function circularText1(txt, radius, classIndex) {
     txt = txt.split(""),
-      classIndex = document.getElementsByClassName("intro__text--holder")[classIndex];
+      classIndex = document.getElementsByClassName("intro__hiIm--holder")[classIndex];
   
-    var deg = 15 / txt.length,
-      origin = 260;
+    var deg = 40 / txt.length,
+      origin = 280;
   
     txt.forEach((ea) => {
-      ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+      ea = `<p class="hiIm" style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
       classIndex.innerHTML += ea;
       origin += deg;
     });
   }
   
-  circularText1("hi, I'm", 280, 0);
+  circularText1("Hi, I'm", 320, 0);
 
 function circularText(txt, radius, classIndex) {
     txt = txt.split(""),
@@ -24,7 +24,7 @@ function circularText(txt, radius, classIndex) {
       origin = 240;
   
     txt.forEach((ea) => {
-      ea = `<h1 style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</h1>`;
+      ea = `<h1 class="circTxt" style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</h1>`;
       classIndex.innerHTML += ea;
       origin += deg;
     });
