163c318d985b8bf8eff1583966a21bccc7c11a45
Chantelle Isaacs <chantelle616@gmail.com>
2020-05-29 22:55:44 -0700

    add curved text around image

--- js/index.js
+++ js/index.js
@@ -1,14 +1,33 @@
-// const navToggle = document.querySelector('.nav-toggle');
-// const navLinks = document.querySelectorAll('.nav__link')
 
-// navToggle.addEventListener('click', () => {
-//     document.body.classList.toggle('nav-open');
-// });
+//makes text curve
+function circularText1(txt, radius, classIndex) {
+    txt = txt.split(""),
+      classIndex = document.getElementsByClassName("intro__text--holder")[classIndex];
+  
+    var deg = 15 / txt.length,
+      origin = 260;
+  
+    txt.forEach((ea) => {
+      ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+      classIndex.innerHTML += ea;
+      origin += deg;
+    });
+  }
+  
+  circularText1("hi, I'm", 280, 0);
 
-// navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//         document.body.classList.remove('nav-open');
-//     })
-// })
-
-
+function circularText(txt, radius, classIndex) {
+    txt = txt.split(""),
+      classIndex = document.getElementsByClassName("intro__text--holder")[classIndex];
+  
+    var deg = 160 / txt.length,
+      origin = 240;
+  
+    txt.forEach((ea) => {
+      ea = `<h1 style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</h1>`;
+      classIndex.innerHTML += ea;
+      origin += deg;
+    });
+  }
+  
+  circularText("Chantelle Isaacs", 270, 0);
